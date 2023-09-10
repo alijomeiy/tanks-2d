d4ad84366401dfedfa59f1012120f57e1a110e4e
bazrafshanmh <bazrafshan.m.h@gmail.com>
2023-09-10 16:32:01 +0330

    Our HomeWork

--- Assets/Scripts/LightningBullet.cs
+++ Assets/Scripts/LightningBullet.cs
@@ -4,33 +4,25 @@ using UnityEngine;
 
 public class LighitngBullet : Bullet
 {
-    public Rigidbody2D _rigidbody;
-    public ParticleSystem Effect;
-    public GameObject Bullet;
-    public float Force = 5;
-    private void Start()
+    private Rigidbody2D _rigidbody;
+    
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        Shoot(new Vector2(20,20));
     }
 
     private void Spawn(GameObject Bullet1,Vector3 Position)
     {
-        Instantiate(Bullet1,Position,Quaternion.identity);
+        GameObject Bullet = Instantiate(Bullet1,Position,Quaternion.identity);
     }
 
     public override void Shoot(Vector2 force)
     {
         _rigidbody.AddForce(force, ForceMode2D.Impulse);
         Vector3 Position = transform.position;
-        Spawn(Bullet,Position*20);
-        Spawn(Bullet,Position*40);
-        Spawn(Bullet,Position*60);
-        Spawn(Bullet,Position*80);
-    }
-    public void OnCollisionEnter2D(Collision2D other)
-    {
-        Instantiate(Effect,transform.position,Quaternion.identity);
-        Destroy(gameObject);
+        Spawn(gameObject,Position*20);
+        Spawn(gameObject,Position*40);
+        Spawn(gameObject,Position*60);
+        Spawn(gameObject,Position*80);
     }
 }
--- Assets/Scripts/ProjectileBullet.cs
+++ Assets/Scripts/ProjectileBullet.cs
@@ -3,24 +3,15 @@ using UnityEngine;
 
 public class ProjectileBullet : Bullet
 {
-    public Rigidbody2D _rigidbody;
-    public ParticleSystem Effect;
-    public float Force = 1;
+    private Rigidbody2D _rigidbody;
 
-
-    private void Start()
+    private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
-        Shoot(new Vector2(2,2));
     }
 
     public override void Shoot(Vector2 force)
     {
         _rigidbody.AddForce(force, ForceMode2D.Impulse);
-    }
-    public void OnCollisionEnter2D(Collision2D other)
-    {
-        Instantiate(Effect,transform.position,Quaternion.identity);
-        Destroy(gameObject);
     }
 }
